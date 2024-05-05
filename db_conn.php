0f54d1e6013307a25ba1afc4f8710294fc817996
Wwissal <96307775+Wwissal@users.noreply.github.com>
2024-05-06 00:46:50 +0100

    OK

--- db_conn.php
+++ db_conn.php
@@ -5,10 +5,14 @@ $password = "Hello@world12";
 $dbname = "gestionotes-database";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Create connection
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
 
 // Check connection
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 // echo "Connected successfully";
+?>
